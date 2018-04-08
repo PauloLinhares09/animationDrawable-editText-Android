1b11fdf4220d270a423c6254feed69d35ea39fbd
Paulo Linhares <paulolinhares@mbp-de-macbook.home>
2018-04-07 23:51:18 -0300

    example

---
+++ app/src/main/java/com/paulolinhares/animedrawable/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.paulolinhares.animedrawable
+
+import android.graphics.drawable.AnimationDrawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var animationCurrent : AnimationDrawable
+    lateinit var before : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        etField.addTextChangedListener(object : TextWatcher{
+
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i("TAG", "init: " + p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("TAG", "before: " + p0.toString())
+                before = p0.toString()
+
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("TAG", "changed: " + p0.toString())
+
+                if(p0!!.length == 1 && before.length < p0.length) {
+                    imageView.setImageDrawable(resources.getDrawable(R.drawable.anim_step1))
+                    animationCurrent = imageView.drawable as AnimationDrawable
+                    animationCurrent.isOneShot = true
+                    animationCurrent.start()
+                }
+                if (p0.length == 0 && before.length > p0.length) {
+                    imageView.setImageDrawable(resources.getDrawable(R.drawable.anim_step1_reverse))
+                    animationCurrent = imageView.drawable as AnimationDrawable
+                    animationCurrent.isOneShot = true
+                    animationCurrent.start()
+                }
+                if (p0.length == 5 && before.length < p0.length){
+                    imageView.setImageDrawable(resources.getDrawable(R.drawable.anim_step2))
+                    animationCurrent = imageView.drawable as AnimationDrawable
+                    animationCurrent.isOneShot = true
+                    animationCurrent.start()
+                }
+                if (p0.length == 5 && before.length > p0.length){
+                    imageView.setImageDrawable(resources.getDrawable(R.drawable.anim_step2_reverse))
+                    animationCurrent = imageView.drawable as AnimationDrawable
+                    animationCurrent.isOneShot = true
+                    animationCurrent.start()
+                }
+
+                //continue
+            }
+
+        })
+
+    }
+
+
+}
